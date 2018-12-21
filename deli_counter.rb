cde01b14c15f17f9b12ed6606a39c3471854868f
Nate Hunter <hunter.j.nate@gmail.com>
2018-12-21 22:15:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,10 +4,9 @@ katz_deli = []
 
 def line (line)
   #katz_deli = []
-  if katz_deli == 0 
+  if katz_deli.count == 0 
     puts "The line is currently empty!"
-  elsif katz_deli.count >= 1 
-  puts "The line is currently: #{katz_deli[0+1]} #{name}"
+ 
   
 end
 end
